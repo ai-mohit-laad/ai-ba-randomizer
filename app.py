7b913c770a71a80be90d133880cad17c1b2c7bd8
ai-mohit-laad <141032731+ai-mohit-laad@users.noreply.github.com>
2024-06-10 05:48:20 -0700

    init commit

---
+++ app.py
@@ -0,0 +1,85 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from send_email_for_crew import send_email_to_crew
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+
+st.image('./air_india_logo.png')
+st.title("BA Randomizer")
+
+with open('./config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['pre-authorized']
+)
+
+authenticator.login()
+
+logged_in = False
+if st.session_state["authentication_status"]:
+    authenticator.logout()
+    st.write(f'Welcome *{st.session_state["name"]}*')
+    logged_in = True
+elif st.session_state["authentication_status"] is False:
+    st.error('Username/password is incorrect')
+elif st.session_state["authentication_status"] is None:
+    st.warning('Please enter your username and password')
+
+if logged_in:
+    # Initialize session state for seed
+    if 'seed' not in st.session_state:
+        st.session_state.seed = np.random.randint(0, 1000000)
+
+    if 'emails_sent' not in st.session_state:
+        st.session_state.emails_sent = False
+
+    uploaded_file = st.file_uploader("Add the List of Staff strictly as per format communicated", type=["xlsx"])
+
+    correct_file_added = False
+    if uploaded_file is not None:
+        try:
+            staff_df = pd.read_excel(uploaded_file)
+            # st.write(df)
+            correct_file_added = True
+        except Exception as e:
+            st.error(f"Error reading file: {e}")
+
+    percentage_input = st.number_input('Enter percentage of staff bound to go through BA test', 
+                                value=25, min_value=0, max_value=100, disabled=correct_file_added==False)
+
+    def randomize_staff(df, percentage):
+        num_rows = len(df)
+        num_samples = max(1, int(num_rows * percentage/100))  
+        
+        random_sample = df.sample(n=num_samples, random_state=st.session_state.seed)
+        return random_sample
+
+    def send_email_to_everyone(randomized_df):
+        print('Sending email to everyone...')
+        for index, row in randomized_df.iterrows():
+            send_email_to_crew(row['Email'], row['Name'])
+            break
+        st.session_state.emails_sent = True
+
+
+    if correct_file_added and percentage_input:
+        randomized_df = randomize_staff(staff_df, percentage_input)
+        st.write(randomized_df)
+
+        if st.button('Send Email to everyone'):
+            send_email_to_everyone(randomized_df)
+        
+        # Change the button label after emails are sent
+        if st.session_state.emails_sent:
+            st.success("Emails have been sent successfully.")
+
+        # st.button('Send Email to everyone', on_click=send_email_to_everyone(randomized_df))
+        
+        
---
+++ send_email_for_crew.py
@@ -0,0 +1,65 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+smtp_server = 'email-smtp.ap-south-1.amazonaws.com'
+smtp_port = 587
+smtp_username = 'AKIA6MPHRG2WGXH7S4WD'
+smtp_password = os.getenv('SES_SMTP_PASSWORD')
+
+def send_email_to_crew(email, name):
+
+    sender = 'ops.tech@airindia.com'
+    
+    name = ' '.join(word.strip().capitalize() for word in name.split())
+    
+    body = f"""
+        <html>
+        <head>
+        </head>
+        <body>
+            <p>Dear {name},</p>
+
+            blah blah blah
+          
+            <p>Thank You, and kind regards</p>
+
+            <p>Operations Tech Team</p>
+
+            <p>Air India D&T</p>
+            
+            <i>Disclaimer: Do not reply, this is an auto generated mail.</i>
+        </body>
+        </html>
+        """
+
+    to_emails = [email]
+    # to_emails = ['testhh@airindia.com']
+    test_emails = [
+        'mohit.laad@airindia.com',
+    ]
+    cc_emails = [
+    ]   
+    
+    message = MIMEMultipart()
+
+    message['From'] = sender
+    message['To'] = ','.join(to_emails)
+    message['Cc'] = ','.join(cc_emails)
+    message['Bcc'] = ','.join(test_emails)
+    message['Subject'] = f'Testing of BA Randomizer'  
+    message.attach(MIMEText(body, 'html'))
+    
+    with smtplib.SMTP(smtp_server, smtp_port) as server:
+        server.starttls()
+        server.login(smtp_username, smtp_password)
+        recipients = to_emails + cc_emails + test_emails  # Combine To and Cc recipients
+        server.sendmail(sender, recipients, message.as_string())
+        server.close()
+        print(f"[ ]     Email sent successfully to {recipients}!")
+    
+
